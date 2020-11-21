8187f71e3e3929aeb196b621884dd469f8d6870a
dromide <mz.hisia@gmail.com>
2020-11-21 23:55:44 +0900

    renderList

--- js/main.js
+++ js/main.js
@@ -4,3 +4,38 @@ import ToDoItem from "./todoitem";
 const ToDoList = new ToDoList();
 
 // Launch app
+document.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  // Add listeners
+
+  // Procedural
+
+  // load list object
+  refreshThePage();
+};
+
+const refreshThePage = () => {
+  clearListDisplay();
+  // renderList();
+  // clearItemEntryField();
+  // setFocusOnItemEntry();
+};
+
+const clearListDisplay = () => {
+  const parentElement = document.getElementById("listItems");
+  deleteContents(parentElement);
+};
+const deleteContents = (parentElement) => {
+  let child = parentElement.lastElementChild;
+  while (child) {
+    parentElement.removeChild(child);
+    child = parentElement.lastElementChild;
+  }
+};
+
+const renderList = () => {};
